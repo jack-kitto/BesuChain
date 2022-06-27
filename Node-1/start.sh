5d4a36bc99928498252aeee6dd05b21527020f56
David Coolahan <thesechips@gmail.com>
2022-06-27 06:56:10 +0100

    changes

--- Node-1/start.sh
+++ Node-1/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-besu --data-path=data --genesis-file=../genesis.json --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-allowlist="*" --rpc-http-cors-origins="all" --rpc-http-host="0.0.0.0"
+besu --data-path=data --genesis-file=./genesis.json --rpc-http-enabled --discovery-enabled=false --p2p-enabled=true --p2p-host=192.168.7.79 --rpc-http-api=ETH,NET,IBFT --host-allowlist="*" --rpc-http-cors-origins="all" --rpc-http-host="0.0.0.0"
 
---
+++ Node-4/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+besu --data-path=data --genesis-file=./genesis.json --rpc-http-enabled --discovery-enabled=false --p2p-enabled=true --p2p-host=192.168.7.78 --rpc-http-api=ETH,NET,IBFT --host-allowlist="*" --rpc-http-cors-origins="all" --rpc-http-host="0.0.0.0"
+
